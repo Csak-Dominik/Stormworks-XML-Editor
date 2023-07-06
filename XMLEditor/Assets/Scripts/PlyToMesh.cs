c9f1bd92f14d353bb8b015efd3cd16a7e2317f14
Csak_Dominik <domi20031022@gmail.com>
2023-07-06 12:46:00 +0200

    changed unity version

---
+++ XMLEditor/Assets/Scripts/PlyToMesh.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlyToMesh : MonoBehaivour
+{
+    public static Mesh Load(string path)
+    {
+        //var ply = new PlyReader(path);
+        //var mesh = new Mesh();
+        //mesh.vertices = ply.Vertices;
+        //mesh.triangles = ply.Triangles;
+        //mesh.normals = ply.Normals;
+        //mesh.colors = ply.Colors;
+        //return mesh;
+        return null;
+    }
+}
